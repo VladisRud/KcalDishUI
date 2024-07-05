2a884e07932e2b6a85de9f1f5b11ad3f6cbac2ef
VladisRud <vladisrud@gmail.com>
2024-07-05 17:48:50 +0400

    Initial Commit

---
+++ KcalDish/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KcalDish
+//
+//  Created by Vlad Rudenko on 05.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KcalDish/KcalDishApp.swift
@@ -0,0 +1,17 @@
+//
+//  KcalDishApp.swift
+//  KcalDish
+//
+//  Created by Vlad Rudenko on 05.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct KcalDishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
